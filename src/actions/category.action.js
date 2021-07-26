6af09623adc69e254aa4c2f7b11e6d420665ae87
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-07-26 20:55:44 +0530

    Flipcart Categories Dropdown

---
+++ src/actions/category.action.js
@@ -0,0 +1,24 @@
+import axios from "../helpers/axios";
+import { categoryConstants } from "./constants";
+
+export const getAllCategory = () => {
+    return async dispatch => {
+        const res = await axios.get('/category/getCategory');
+        console.log(res);
+        dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST });
+        if (res.status == 200) {
+
+            const { categoryList } = res.data;
+            dispatch({
+                type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
+                payload: { categories: categoryList }
+            });
+        } else {
+            dispatch({
+                type: categoryConstants.GET_ALL_CATEGORIES_FAILURE,
+                payload: { error: res.data.error }
+            });
+        }
+    }
+}
+
