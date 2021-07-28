9b9c83addfbd9845c304019a5182a939dbd55fab
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-07-28 17:36:52 +0530

    fetch product API & render product list

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header'
+import Layout from '../../components/Layout'
 import MenuHeader from '../../components/MenuHeader'
 
 function HomePage() {
     return (
-        <div>
-          <Header />
-          <MenuHeader />
-        </div>
+      <Layout>
+        Home Page
+      </Layout>
     )
 }
 
